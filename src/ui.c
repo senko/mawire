cbf297f0a1fe189687bb55a507a2890efaec554c
Senko Rasic <senko.rasic@collabora.co.uk>
2010-01-11 23:54:36 +0100

    disabled Random button until we implement the functionality

--- src/ui.c
+++ src/ui.c
@@ -417,6 +417,9 @@ show_main_window (GCallback settings_cb, GCallback about_cb,
 
   g_signal_connect (G_OBJECT (search_btn), "clicked",
       G_CALLBACK (search_clicked_cb), window);
+
+  /* We haven't implemented Random yet, don't confuse the user. */
+  gtk_widget_set_sensitive (random_btn, FALSE);
   g_signal_connect (G_OBJECT (random_btn), "clicked",
       G_CALLBACK (random_clicked_cb), window);
 
